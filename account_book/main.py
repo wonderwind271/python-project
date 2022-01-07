8616be082c631c2516e0dc66b415384c8e04c826
wonderwind <wushuyu2002@sjtu.edu.cn>
2022-01-07 15:43:11 +0800

    row insert function

--- account_book/main.py
+++ account_book/main.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
 
+def row_insert(df, i, df_add):
+    # 指定第i行插入一行数据
+    df1 = df.iloc[:i, :]
+    df2 = df.iloc[i:, :]
+    df_new = pd.concat([df1, df_add, df2], ignore_index=True)
+    return df_new
+
+
+
 records=pd.read_csv('account.csv',names=['item','price','date','method','note'])
 # print(records.loc[0])
 records_data=np.array(records)
@@ -10,6 +19,8 @@ print("successfully read data, please input commands. Input help to get a manual
 
 help_list=[]
 cmd=""
+print(records)
+
 while cmd!='q':
     cmd=input("> ")
     # start processing
@@ -17,6 +28,8 @@ while cmd!='q':
         print(help_list)
     elif cmd=='l' or cmd=='list':
         print(records.to_string())
+    elif cmd=='add':
+        print('creating new record...')
 
 
 
